86767a4fcd972593e2692e194c441c17ae90629c
chris.hmc <chris.hmc@link8tech.tw>
2020-11-20 16:31:11 +0800

    [新增] 新增 firebase 處理登入動作

--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -5,14 +5,55 @@ import { apiLogin } from '@apis';
 import { disPatchLoginInfo } from '@actions/userAction';
 import { useDispatch } from 'react-redux';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import { FirebaseAuthProvider } from '@react-firebase/auth';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyAncpQXcc6UAlllACYvqDzrpQnemgsDKbk',
+    authDomain: 'member-database-fdf1e.firebaseapp.com',
+    databaseURL: 'https://member-database-fdf1e.firebaseio.com',
+    projectId: 'member-database-fdf1e',
+    storageBucket: 'member-database-fdf1e.appspot.com',
+    messagingSenderId: '398211092204',
+    appId: '1:398211092204:web:e56a27f2a566c36e69dfc9',
+    measurementId: 'G-19CNHH96GK',
+};
+
 function Login() {
     const dispatch = useDispatch();
 
     const onFinish = (values) => {
-        apiLogin().then(function (response) {
-            // handle success
-            dispatch(disPatchLoginInfo(response.data));
-        });
+        /*firebase方式*/
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(values.username, values.password)
+            .then(() => {
+                dispatch(
+                    disPatchLoginInfo({
+                        success: true,
+                        type: '測試測試',
+                    })
+                );
+                alert('恭喜 登入成功');
+            })
+            .catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                if (errorCode === 'auth/wrong-password') {
+                    alert('Wrong password.');
+                } else {
+                    alert(errorMessage);
+                }
+                console.log(error);
+            });
+
+        /*call Yapi方式*/
+        // apiLogin().then(function (response) {
+        //     // handle success
+        //     dispatch(disPatchLoginInfo(response.data));
+        // });
         console.log('Success:', values);
     };
 
@@ -21,51 +62,53 @@ function Login() {
     };
 
     return (
-        <Container>
-            <div className="board-container"></div>
-            <Form
-                name="basic"
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-            >
-                <Form.Item
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: '請輸入帳號!',
-                        },
-                    ]}
+        <FirebaseAuthProvider {...firebaseConfig} firebase={firebase}>
+            <Container>
+                <div className="board-container"></div>
+                <Form
+                    name="basic"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
                 >
-                    <Input placeholder="帳號" />
-                </Form.Item>
-
-                <Form.Item
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: '請輸入密碼!',
-                        },
-                    ]}
-                >
-                    <Input.Password placeholder="密碼" />
-                </Form.Item>
-
-                <Form.Item>
-                    <Button
-                        className="submit-button"
-                        type="primary"
-                        htmlType="submit"
+                    <Form.Item
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: '請輸入帳號!',
+                            },
+                        ]}
                     >
-                        登入
-                    </Button>
-                </Form.Item>
-            </Form>
-        </Container>
+                        <Input placeholder="帳號" />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: '請輸入密碼!',
+                            },
+                        ]}
+                    >
+                        <Input.Password placeholder="密碼" />
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button
+                            className="submit-button"
+                            type="primary"
+                            htmlType="submit"
+                        >
+                            登入
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Container>
+        </FirebaseAuthProvider>
     );
 }
 
